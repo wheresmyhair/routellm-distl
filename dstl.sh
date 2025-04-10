4a339c8eba179e5dfdb88a56e6f222ea6005a5e8
YizhenJia <yizhen.jia96@gmail.com>
2025-04-10 14:26:54 +0800

    port

--- dstl.sh
+++ dstl.sh
@@ -1,7 +1,7 @@
 all_tasks=bbh,truthfulqa,agieval,mmlu,ifeval,arc_easy,arc_challenge,hellaswag,openbookqa,piqa,social_iqa,winogrande,wikitext,gpqa,minerva_math,gsm8k,humaneval,mbpp
 
 HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=0 nohup lm_eval --model sglang \
-    --model_args pretrained=meta-llama/Llama-3.2-3B-Instruct,dtype=auto \
+    --model_args pretrained=meta-llama/Llama-3.2-3B-Instruct,dtype=auto,port=12330 \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \
@@ -11,7 +11,7 @@ HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=0 nohup lm_eval --model sglang \
     --confirm_run_unsafe_code > ./allres/llama3.2-3b.log 2>&1 &
 
 HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=1 nohup lm_eval --model sglang \
-    --model_args pretrained=meta-llama/Llama-3.2-1B-Instruct,dtype=auto \
+    --model_args pretrained=meta-llama/Llama-3.2-1B-Instruct,dtype=auto,port=12331 \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \
@@ -21,7 +21,7 @@ HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=1 nohup lm_eval --model sglang \
     --confirm_run_unsafe_code > ./allres/llama3.2-1b.log 2>&1 &
 
 HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=2 nohup lm_eval --model sglang \
-    --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,dtype=auto \
+    --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,dtype=auto,port=12332 \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \
@@ -31,7 +31,7 @@ HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=2 nohup lm_eval --model sglang \
     --confirm_run_unsafe_code > ./allres/llama3.1-8b.log 2>&1 &
 
 HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=3 nohup lm_eval --model sglang \
-    --model_args pretrained=google/gemma-3-1b-it,dtype=auto \
+    --model_args pretrained=google/gemma-3-1b-it,dtype=auto,port=12333 \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \
@@ -41,7 +41,7 @@ HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=3 nohup lm_eval --model sglang \
     --confirm_run_unsafe_code > ./allres/gemma-3-1b.log 2>&1 &
 
 HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=4 nohup lm_eval --model sglang \
-    --model_args pretrained=google/gemma-3-4b-it,dtype=auto \
+    --model_args pretrained=google/gemma-3-4b-it,dtype=auto,port=12334 \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \

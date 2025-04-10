16e03557b33756f47c4d7c8a15a3a006d31c7db3
YizhenJia <yizhen.jia96@gmail.com>
2025-04-10 14:12:29 +0800

    sgl

--- dstl.sh
+++ dstl.sh
@@ -1,7 +1,7 @@
 all_tasks=bbh,truthfulqa,agieval,mmlu,ifeval,arc_easy,arc_challenge,hellaswag,openbookqa,piqa,social_iqa,winogrande,wikitext,gpqa,minerva_math,gsm8k,humaneval,mbpp
 
-HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=0 nohup lm_eval --model vllm \
-    --model_args pretrained=meta-llama/Llama-3.2-3B-Instruct,dtype=auto,gpu_memory_utilization=0.5 \
+HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=0 nohup lm_eval --model sglang \
+    --model_args pretrained=meta-llama/Llama-3.2-3B-Instruct,dtype=auto \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \
@@ -10,8 +10,8 @@ HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=0 nohup lm_eval --model vllm \
     --log_samples \
     --confirm_run_unsafe_code > ./allres/llama3.2-3b.log 2>&1 &
 
-HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=1 nohup lm_eval --model vllm \
-    --model_args pretrained=meta-llama/Llama-3.2-1B-Instruct,dtype=auto,gpu_memory_utilization=0.5 \
+HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=1 nohup lm_eval --model sglang \
+    --model_args pretrained=meta-llama/Llama-3.2-1B-Instruct,dtype=auto \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \
@@ -20,8 +20,8 @@ HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=1 nohup lm_eval --model vllm \
     --log_samples \
     --confirm_run_unsafe_code > ./allres/llama3.2-1b.log 2>&1 &
 
-HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=2 nohup lm_eval --model vllm \
-    --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,dtype=auto,gpu_memory_utilization=0.5 \
+HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=2 nohup lm_eval --model sglang \
+    --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,dtype=auto \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \
@@ -30,8 +30,8 @@ HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=2 nohup lm_eval --model vllm \
     --log_samples \
     --confirm_run_unsafe_code > ./allres/llama3.1-8b.log 2>&1 &
 
-HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=3 nohup lm_eval --model vllm \
-    --model_args pretrained=google/gemma-3-1b-it,dtype=auto,gpu_memory_utilization=0.5 \
+HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=3 nohup lm_eval --model sglang \
+    --model_args pretrained=google/gemma-3-1b-it,dtype=auto \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \
@@ -40,8 +40,8 @@ HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=3 nohup lm_eval --model vllm \
     --log_samples \
     --confirm_run_unsafe_code > ./allres/gemma-3-1b.log 2>&1 &
 
-HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=4 nohup lm_eval --model vllm \
-    --model_args pretrained=google/gemma-3-4b-it,dtype=auto,gpu_memory_utilization=0.5 \
+HF_ALLOW_CODE_EVAL="1" CUDA_VISIBLE_DEVICES=4 nohup lm_eval --model sglang \
+    --model_args pretrained=google/gemma-3-4b-it,dtype=auto \
     --tasks $all_tasks \
     --batch_size 1 \
     --cache_requests refresh \
